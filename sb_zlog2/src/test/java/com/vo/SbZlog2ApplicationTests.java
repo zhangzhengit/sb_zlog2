747ddd693de2e2aa47cc05287441760dc85ddd91
zhangzhen <1071324756@qq.com>
2024-05-31 13:16:24 +0800

    加入默认值 zlog.file.fileSize

--- sb_zlog2/src/test/java/com/vo/SbZlog2ApplicationTests.java
+++ sb_zlog2/src/test/java/com/vo/SbZlog2ApplicationTests.java
@@ -1,29 +1,26 @@
 package com.vo;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import com.vo.core.ZLog2;
-
-import cn.hutool.log.Log;
 
 //@SpringBootTest
 class SbZlog2ApplicationTests {
 
 	static final ZLog2 LOG = ZLog2.getInstance();
 
-
 	@Test
 	void contextLoads() {
-		System.out.println(java.time.LocalDateTime.now() + "\t" + Thread.currentThread().getName() + "\t"
+		System.out.println(Thread.currentThread().getName() + "\t" + LocalDateTime.now() + "\t"
 				+ "SbZlog2ApplicationTests.contextLoads()");
 
 
 		final int i = 10000 * 20;
 
 		for (int n = 1; n <= i; n++) {
-			LOG.info("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+			LOG.info("这是行数={}", n);
 		}
 	}
 
